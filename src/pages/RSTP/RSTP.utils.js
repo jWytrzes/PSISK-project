374b02c9c97da8429ea368756045a386e456c53a
KonradWrbl <160815@stud.prz.edu.pl>
2021-05-24 20:27:42 +0200

    update

--- src/pages/RSTP/RSTP.utils.js
+++ src/pages/RSTP/RSTP.utils.js
@@ -20,7 +20,7 @@ export const RSTPUtils = [
 		align: 'justify',
 		paragraph: true,
 		content:
-			'Jak wspomniano wyżej RSTP (IEEE 802.1w) jest rozszerzeniem sieciowego protokołu STP (IEEE 802.1D). Standard STP został stworzony w czasie, gdy przywrócenie działania sieci po okresie braku zasilania w ciegu jednej minuty było wystarczająco szybkie i wydajne. Z czasem potrzeby jednak wzrosły i w tym momencie z pomocą przyszedł nowo utworzony standard RSTP. W nowym standardzie mostkowanie konkuruje ze starymi rozwiązaniami routowanymi. Użyte w nim protkoły, takie jak Open Shortest Path First (OSPF) i Enhanced Interior Gateway Routing (EIGRP), są w stanie zapewnić alternatywną ścieżkę w znacznie krótszym czasie. Firma Cisco wprowadzając ulepszenia poprzedniej, oryginalnej specyfikacjie 802.1D wprowadziła funkcje przyspieszające czas konwergencji sieci z mostem. Są to między innymi: Uplink Fast, Backbone Fast i port Fast. Za wadę nowych mechanizmów można uznać to, że są one zastrzeżone i wymagają dodatkowej konfiguracji. Ze względu na małą liczbę zmian RSTP względem STP, nowy protokuł (RSTP, IEEE 802.1w) można uznać raczej za ewolucję poprzedniego, a nie rewolucję. Terminologia 802.1d pozostaje zasadniczo taka samo, a więszkość parametrów pozostała niezmieniona. Dzięki temu, nauka nowego standardu dla osób dobrze zaznajomionych z poprzednim jest łatwa i przyjemna, a skonfigurowanie nowego protokołu nie powinno stanowić dla nich problemu. W większości przypadków RSTP działa lepiej niż zastrzeżone rozszerzenia Cisco bez dodatkowej konfiguracji. W standardzie 802.1w można również powrócić do standardu 802.1D w celu współdziałania ze starszymi mostami w oparciu o port.',
+			'Jak wspomniano wyżej RSTP (IEEE 802.1w) jest rozszerzeniem sieciowego protokołu STP (IEEE 802.1D). Standard STP został stworzony w czasie, gdy przywrócenie działania sieci po okresie braku zasilania w ciegu jednej minuty było wystarczająco szybkie i wydajne. Z czasem potrzeby jednak wzrosły i w tym momencie z pomocą przyszedł nowo utworzony standard RSTP. W nowym standardzie mostkowanie konkuruje ze starymi rozwiązaniami routowanymi. Użyte w nim protkoły, takie jak Open Shortest Path First (OSPF) i Enhanced Interior Gateway Routing (EIGRP), są w stanie zapewnić alternatywną ścieżkę w znacznie krótszym czasie. Firma Cisco wprowadzając ulepszenia poprzedniej, oryginalnej specyfikacjie 802.1D wprowadziła funkcje przyspieszające czas konwergencji sieci z mostem. Są to między innymi: Uplink Fast, Backbone Fast i port Fast. Za wadę nowych mechanizmów można uznać to, że są one zastrzeżone i wymagają dodatkowej konfiguracji. Ze względu na małą liczbę zmian RSTP względem STP, nowy protokuł (RSTP, IEEE 802.1w) można uznać raczej za ewolucję poprzedniego, a nie rewolucję. Terminologia 802.1d pozostaje zasadniczo taka samo, a więszkość parametrów pozostała niezmieniona. Dzięki temu, nauka nowego standardu dla osób dobrze zaznajomionych z poprzednim jest łatwa i przyjemna, a skonfigurowanie nowego protokołu nie powinno stanowić dla nich problemu. W większości przypadków RSTP działa lepiej niż zastrzeżone rozszerzenia Cisco bez dodatkowej konfiguracji. W standardzie 802.1w można również powrócić do standardu 802.1D w celu współdziałania ze starszymi mostami w oparciu o port. [1]',
 	},
 	{
 		header: 'Nowe stany i role portów',
@@ -43,16 +43,17 @@ export const RSTPUtils = [
 					Stan portu jest mieszany, niezależnie od tego, czy blokuje, czy
 					przekazuje ruch, oraz rolę, jaką odgrywa w aktywnej topologii (port
 					główny, port wyznaczony itd.). Na przykład z operacyjnego punktu
-					widzenia nie ma różnicy między portem w stanie blokowania a portem w
+					widzenia nie ma różnicy między portem w stanie blokowania, a portem w
 					stanie nasłuchiwania. Oba odrzucają ramki i nie uczą się adresów MAC.
 					Prawdziwa różnica polega na roli, jaką drzewo opinające przypisuje
-					portowi. Można bezpiecznie założyć, że port nasłuchujący jest
-					wyznaczony lub root i jest w drodze do stanu przekazywania. Niestety,
-					po przejściu do stanu przekazywania nie ma możliwości wywnioskowania
-					ze stanu portu, czy port jest portem głównym, czy wyznaczonym.
-					Przyczynia się to do wykazania niepowodzenia tej terminologii
-					państwowej. RSTP oddziela rolę i stan portu, aby rozwiązać ten
-					problem.
+					portowi. Można bezpiecznie założyć, że port "listening" jest
+					wyznaczony lub root i jest w drodze do stanu "forwarding". Niestety,
+					po przejściu do stanu "forwarding" nie ma możliwości wywnioskowania ze
+					stanu portu, czy port jest portem głównym, czy wyznaczonym. Przyczynia
+					się to do wykazania niepowodzenia tej terminologii państwowej. RSTP
+					oddziela rolę i stan portu, aby rozwiązać ten problem. Więcej
+					informacji na temat tych portów omówiono w częśći poświęconej{' '}
+					<a href="/stp">STP</a>. [1]
 				</p>
 			</>
 		),
@@ -66,32 +67,33 @@ export const RSTPUtils = [
 			<>
 				<p>
 					<b>Role portów głównych</b> - Port, który odbiera najlepszą jednostkę
-					BPDU na moście, jest portem głównym. Jest to port znajdujący się
-					najbliżej mostu głównego pod względem kosztu ścieżki. STA wybiera
-					pojedynczy most główny w całej zmostkowanej sieci (dla każdej sieci
-					VLAN). Most główny wysyła jednostki BPDU, które są bardziej przydatne
-					niż te, które wysyła każdy inny most. Most główny jest jedynym mostem
-					w sieci, który nie ma portu głównego. Wszystkie inne mosty odbierają
-					jednostki BPDU na co najmniej jednym porcie.
+					BPDU na moście, jest portem głównym (Root Port, RP). Znajduje się
+					najbliżej mostu głównego (Root Bridge) pod względem kosztu ścieżki.
+					STP wybiera pojedynczy most główny w całej zmostkowanej sieci (dla
+					każdej sieci VLAN). Most główny wysyła jednostki BPDU, które są
+					bardziej przydatne niż te, które wysyła każdy inny most. Most główny
+					jest jedynym mostem w sieci, który nie ma portu głównego. Wszystkie
+					inne mosty odbierają jednostki BPDU na co najmniej jednym porcie. [1]
 				</p>
 				<img src={role1} alt="role1"></img>
 				<p>
-					<b>Role portów wyznaczonych</b> - Port jest wyznaczony, jeśli może
-					wysłać najlepszą jednostkę BPDU w segmencie, do którego jest
-					podłączony. Mosty 802.1D łączą ze sobą różne segmenty, takie jak
-					segmenty sieci Ethernet, w celu utworzenia domeny z mostkiem. W danym
-					segmencie może istnieć tylko jedna ścieżka prowadząca do mostu
-					głównego. Jeśli są dwa, w sieci istnieje pętla pomostowa. Wszystkie
-					mosty podłączone do danego segmentu nasłuchują jednostek BPDU każdego
-					z nich i uzgadniają most, który wysyła najlepszą jednostkę BPDU, jako
-					most wyznaczony dla tego segmentu. Port na tym moście, który
-					odpowiada, jest portem wyznaczonym dla tego segmentu.
+					<b>Role portów wyznaczonych</b> - Port jest wyznaczony (designated),
+					jeśli ma możlwość by wysłać najlepszą jednostkę BPDU w segmencie, do
+					którego jest podłączony. Mosty 802.1D łączą ze sobą różne segmenty,
+					takie jak segmenty sieci Ethernet, w celu utworzenia domeny z
+					mostkiem. W danym segmencie może istnieć tylko jedna ścieżka
+					prowadząca do mostu głównego. Jeśli są dwa, w sieci istnieje pętla
+					pomostowa. Wszystkie mosty podłączone do danego segmentu nasłuchują
+					jednostek BPDU każdego z nich i uzgadniają most, który wysyła
+					najlepszą jednostkę BPDU, jako most wyznaczony dla tego segmentu. Port
+					na tym moście, który odpowiada, jest portem wyznaczonym dla tego
+					segmentu. [1]
 				</p>
 				<img src={role2} alt="role2"></img>
 				<p>
 					<b>Role portów alternatywnych i zapasowych</b> - Te dwie role
-					odpowiadają stanowi blokowania 802.1D. Zablokowany port jest
-					definiowany jako port inny niż wyznaczony czy główny. Odbiera on
+					odpowiadają stanowi blokowania (discarding) 802.1D. Zablokowany port
+					jest definiowany jako port inny niż wyznaczony czy główny. Odbiera on
 					bardziej użyteczną jednostkę BPDU niż ta, którą wysyła w swoim
 					segmencie. Port alternatywny odbiera bardziej przydatne jednostki BPDU
 					z innego mostu, jest portem zablokowanym:
@@ -110,7 +112,7 @@ export const RSTPUtils = [
 					dlatego może zastąpić port główny w przypadku awarii. Oczywiście port
 					zapasowy zapewnia nadmiarową łączność z tym samym segmentem i nie może
 					zagwarantować alternatywnej łączności z mostem głównym. Dlatego jest
-					wykluczony z grupy łącza w górę.
+					wykluczony z grupy łącza w górę. [1]
 				</p>
 			</>
 		),
@@ -125,14 +127,14 @@ export const RSTPUtils = [
 				<p>
 					RSTP wprowadził kilka zmian do formatu BPDU. Tylko dwie flagi, zmiana
 					topologii (TC) i potwierdzenie TC (TCA), są zdefiniowane w standardzie
-					802.1D. RSTP wykorzystuje teraz wszystkie sześć bitów bajtu flagi.
+					802.1D. RSTP wykorzystuje teraz wszystkie sześć bitów bajtu flagi. [1]
 				</p>
 				<img src={BDPU1} alt="BDPU1"></img>
 			</>
 		),
 	},
 	{
-		header: 'Pełny widok diagramów Cisco BPDU, IEEE BPDU i BPDU',
+		header: 'Pełny widok diagramów Cisco BPDU, IEEE BPDU i BPDU [1]',
 		variant: 'body1',
 		align: 'justify',
 		paragraph: true,
@@ -145,7 +147,9 @@ export const RSTPUtils = [
 		paragraph: true,
 		content: (
 			<a href="https://www.cisco.com/c/en/us/support/docs/lan-switching/spanning-tree-protocol/24062-146.html">
-				Understanding Rapid Spanning Tree Protocol (802.1w) - Cisco
+				[1]
+				www.cisco.com/c/en/us/support/docs/lan-switching/spanning-tree-protocol/24062-146.html
+				[dostęp: 10.05.2021]
 			</a>
 		),
 	},
